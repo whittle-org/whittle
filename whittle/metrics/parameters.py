10298b7ddd2d2758f85e5a43d7978a2a28238e4d
Aaron <kleiaaro@gmail.com>
2024-07-19 10:42:57 +0200

    compute params subnet

--- whittle/metrics/parameters.py
+++ whittle/metrics/parameters.py
@@ -1,39 +1,54 @@
+from lobotomy.models.gpt import GPT
+from lobotomy.models.gpt.blocks import CausalSelfAttention
+from lobotomy.modules.embedding import Embedding
+from lobotomy.modules.linear import Linear
+from lobotomy.modules.layernorm import LayerNorm
+
+
 def compute_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-
-def params_linear_layer(layer):
+def params_linear_layer(layer: Linear):
     params = layer.sub_network_in_features * layer.sub_network_out_features
     if layer.use_bias:
         params += layer.sub_network_out_features
     return params
 
 
-def params_embedding_layer(embedding):
+def params_embedding_layer(embedding: Embedding):
     return embedding.num_embeddings * embedding.sub_network_embedding_dim
 
 
-def params_layer_norm(layer_norm):
+def params_layer_norm(layer_norm: LayerNorm):
     return 2 * layer_norm.sub_network_in_features
 
 
-def params_attention_layer(attention):
+def params_attention_layer(attention: CausalSelfAttention):
+    # TODO: generalize to QHA
     dmodel = attention.sub_network_n_embd
     dhead = attention.sub_network_head_size
     num_heads = attention.sub_network_n_head
     n_attention = (dmodel * dhead + dhead) * num_heads * 3
     n_attention += dmodel * dmodel + dmodel  # output
-    print(num_heads, n_attention)
+
     return n_attention
 
 
-def compute_parameters_sub_network(model):
+def compute_parameters_sub_network_gpt(model: GPT):
 
     """
-    Computes parameters of a selected sub-network.
-    For formulas, see: https://towardsdatascience.com/how-to-estimate-the-number-of-parameters-in-transformer-models-ca0f57d8dff0
+    Computes parameters of the current sub-network of a GPT mmodel. Make sure to set the sub-network before
+    calling this function.
 
+    Refs:
+        https://towardsdatascience.com/how-to-estimate-the-number-of-parameters-in-transformer-models-ca0f57d8dff0
+
+    Args:
+        model: GPT model
+
+    Returns:
+        float: number of parameters of the activated sub-network
     """
 
     num_params = 0
@@ -46,5 +61,5 @@ def compute_parameters_sub_network(model):
         num_params += params_linear_layer(block.mlp.fc)
         num_params += params_linear_layer(block.mlp.proj)
         num_params += params_layer_norm(block.norm_2)
-
+    num_params += params_layer_norm(model.transformer.ln_f)
     return num_params
