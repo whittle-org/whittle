fdee89676052f111de4a68602cc849bc2a3634a2
Aaron Klein <kleiaaro@gmail.com>
2024-08-01 13:04:50 +0200

    Functionality to compute parameters of subnetworks (#78)

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -1,9 +1,71 @@
-from whittle.metrics.parameters import compute_parameters
+import pytest
+
+from litgpt.config import Config
+
+from whittle.metrics.parameters import (
+    compute_parameters,
+    compute_parameters_sub_network_gpt,
+)
+from whittle.models.gpt import GPT
 
 from test.test_training_strategies import MLP
 
 
+mlp_types = ["GptNeoxMLP", "LLaMAMLP", "GemmaMLP"]
+norm_types = ["LayerNorm", "RMSNorm"]
+
+
 def test_compute_parameters():
     model = MLP(8)
-
     assert compute_parameters(model) == 641
+
+
+@pytest.mark.parametrize("mlp_type", mlp_types)
+@pytest.mark.parametrize("norm_type", norm_types)
+def test_compute_parameters_sub_network(mlp_type, norm_type):
+    config = Config()
+    config.padded_vocab_size = 512
+    config.n_embd = 64
+    config.intermediate_size = 64 * 4
+    config.n_head = 8
+    config.n_query_groups = 8
+    config.head_size = 8
+    config.n_layer = 2
+    config.block_size = 512
+    config.rope_n_elem = int(config.rotary_percentage * config.head_size)
+    config.norm_eps = 1e-5
+    config.lm_head_bias = True
+    config.fix_head_size = True
+    config.mlp_class_name = mlp_type
+    config.norm_class_name = norm_type
+
+    gpt = GPT(config)
+
+    params_super_network = compute_parameters(gpt)
+
+    params_sub_network = compute_parameters_sub_network_gpt(gpt)
+    assert params_sub_network == params_super_network
+
+    # reduce super-network by one single head
+    gpt.set_sub_network(
+        sub_network_n_embd=config.n_embd,
+        sub_network_intermediate_size=[
+            config.intermediate_size for _ in range(config.n_layer)
+        ],
+        sub_network_num_heads=[config.n_head - 1]
+        + [config.n_head for _ in range(1, config.n_layer)],
+        sub_network_n_layers=config.n_layer,
+    )
+    params_sub_network = compute_parameters_sub_network_gpt(gpt)
+    params_single_head = (config.n_embd * config.head_size + config.head_size) * 3
+    assert params_sub_network == params_super_network - params_single_head
+
+    # remove larger part of the network
+    gpt.set_sub_network(
+        sub_network_n_embd=config.n_embd // 2,
+        sub_network_intermediate_size=[config.intermediate_size] * config.n_layer,
+        sub_network_num_heads=[config.n_head - 5] * config.n_layer,
+        sub_network_n_layers=config.n_layer - 2,
+    )
+    params_sub_network = compute_parameters_sub_network_gpt(gpt)
+    assert params_sub_network < params_super_network
--- whittle/metrics/parameters.py
+++ whittle/metrics/parameters.py
@@ -1,2 +1,92 @@
+import logging
+
+import torch.nn as nn
+
+from whittle.models.gpt import GPT
+from whittle.models.gpt.blocks import CausalSelfAttention
+from whittle.modules.embedding import Embedding
+from whittle.modules.linear import Linear
+from whittle.modules.layernorm import LayerNorm
+from whittle.modules.rmsnorm import RMSNorm
+from whittle.models.gpt.blocks import GptNeoxMLP, GemmaMLP, LLaMAMLP
+
+
 def compute_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def params_linear_layer(layer: Linear):
+    params = layer.sub_network_in_features * layer.sub_network_out_features
+    if layer.use_bias:
+        params += layer.sub_network_out_features
+    return params
+
+
+def params_embedding_layer(embedding: Embedding):
+    return embedding.num_embeddings * embedding.sub_network_embedding_dim
+
+
+def params_layer_normalization(normalization_layer: nn.Module):
+    if isinstance(normalization_layer, LayerNorm):
+        return 2 * normalization_layer.sub_network_in_features
+    elif isinstance(normalization_layer, RMSNorm):
+        return normalization_layer.sub_network_in_features
+    else:
+        logging.error(
+            f"Normalization layer type: {type(normalization_layer)} not supported!"
+        )
+        raise
+
+
+def params_attention_layer(attention: CausalSelfAttention):
+    # TODO: generalize to QHA
+    dmodel = attention.sub_network_n_embd
+    dhead = attention.sub_network_head_size
+    num_heads = attention.sub_network_n_head
+    n_attention = (dmodel * dhead + dhead) * num_heads * 3
+    n_attention += dmodel * dmodel + dmodel  # output
+
+    return n_attention
+
+
+def params_mlp(mlp: nn.Module):
+    layers = []
+    if isinstance(mlp, GptNeoxMLP):
+        layers = [mlp.proj, mlp.fc]
+
+    elif isinstance(mlp, LLaMAMLP) or isinstance(mlp, GemmaMLP):
+        layers = [mlp.proj, mlp.fc_1, mlp.fc_2]
+
+    num_params = 0
+    for layer in layers:
+        num_params += params_linear_layer(layer)
+    return num_params
+
+
+def compute_parameters_sub_network_gpt(model: GPT):
+    """
+    Computes parameters of the current sub-network of a GPT mmodel. Make sure to set the sub-network before
+    calling this function.
+
+    Refs:
+        https://towardsdatascience.com/how-to-estimate-the-number-of-parameters-in-transformer-models-ca0f57d8dff0
+
+    Args:
+        model: GPT model
+
+    Returns:
+        float: number of parameters of the activated sub-network
+    """
+
+    num_params = 0
+    num_params += params_linear_layer(model.lm_head)
+    num_params += params_embedding_layer(model.transformer.wte)
+    for i in range(model.sub_network_n_layers):
+        block = model.transformer.h[i]
+        num_params += params_mlp(block.mlp)
+        num_params += params_attention_layer(block.attn)
+
+        num_params += params_layer_normalization(block.norm_1)
+        num_params += params_layer_normalization(block.norm_2)
+    num_params += params_layer_normalization(model.transformer.ln_f)
+    return num_params
