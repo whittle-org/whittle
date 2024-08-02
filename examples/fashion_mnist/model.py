a420f2ea4cd65bcfef26763e73d107ae239d2db0
Timur M. Carstensen <timurcarstensen@gmail.com>
2024-08-02 15:38:59 +0200

    updating example with argparser and pretty printing results

--- examples/fashion_mnist/model.py
+++ examples/fashion_mnist/model.py
@@ -15,176 +15,44 @@ class LeNet(nn.Module):
     def __init__(self):
         super().__init__()
         # 1 input image channel, 6 output channels, 5x5 square conv kernel
-        self.fc_base = Linear(28 * 28, 256)
-        self.fc1 = Linear(256, 120)  # 5x5 image dimension
-        self.fc2 = Linear(120, 84)
-        self.fc3 = Linear(84, 10)
+        self.fc_base = Linear(28 * 28, 256, bias=True)
+        self.fc1 = Linear(256, 120, bias=True)  # 5x5 image dimension
+        self.fc2 = Linear(120, 84, bias=True)
+        self.fc3 = Linear(84, 10, bias=True)
 
-    def forward_dense_lottery(
-        self, x, fc1_out, fc2_out, sample_last: bool | None = False
-    ):
+    def forward_dense_lottery(self, x, fc1_out, fc2_out):
         x = x.reshape(x.shape[0], -1)
         x = self.fc_base(x)
         x = F.relu(x)
-        if sample_last:
-            start1 = 120 - fc1_out
-            start2 = 84 - fc2_out
-        else:
-            start1 = 0
-            start2 = 0
-        # TODO: refactor this into a function since it is used in forward_dense_random_ind
-
-        # x = self.fc1.set_sub_network(sub_network_in_features=)
-        x = F.linear(
-            x,
-            weight=self.fc1.weight[start1 : start1 + fc1_out, :],
-            bias=self.fc1.bias[start1 : start1 + fc1_out],
+        self.fc1.set_sub_network(
+            sub_network_in_features=256, sub_network_out_features=fc1_out
         )
+        x = self.fc1(x)
+        self.fc1.reset_super_network()
         x = F.relu(x)
 
-        x = F.linear(
-            x,
-            weight=self.fc2.weight[
-                start2 : start2 + fc2_out, start1 : start1 + fc1_out
-            ],
-            bias=self.fc2.bias[start2 : start2 + fc2_out],
+        self.fc2.set_sub_network(
+            sub_network_in_features=fc1_out,
+            sub_network_out_features=fc2_out,
         )
+        x = self.fc2(x)
+        self.fc2.reset_super_network()
         x = F.relu(x)
 
-        x = F.linear(
-            x, weight=self.fc3.weight[:, start2 : start2 + fc2_out], bias=self.fc3.bias
+        self.fc3.set_sub_network(
+            sub_network_in_features=fc2_out, sub_network_out_features=10
         )
-
+        x = self.fc3(x)
+        self.fc3.reset_super_network()
         return x
 
     @staticmethod
     def sample_ids(max_id: int, num_ids: int):
         return np.random.choice(np.arange(max_id), num_ids)
 
-    def forward_dense_random_neurons(self, x, fc1_out, fc2_out, seed=1234):
-        """
-        # TODO: replace these individual forwards with calls to lobotomy.modules.Linear
-
-        Randomly samples a subset of neurons from each layer (i.e., a sub-network) and computes the forward pass.
-        Args:
-            x: input tensor
-            fc1_out:
-            fc2_out:
-            seed:
-
-        Returns:
-
-        """
-        np.random.seed(seed)
-        x = x.reshape(x.shape[0], -1)
-        # TODO: in the case of fc1_out=120 and fc2_out=84, this will sample all neurons, but currently with replacement
-        fc1_ids = self.sample_ids(max_id=120, num_ids=fc1_out)
-        fc2_ids = self.sample_ids(max_id=84, num_ids=fc2_out)
-
-        x = self.fc_base(x)
-        x = F.relu(x)
-        x = F.linear(x, weight=self.fc1.weight[fc1_ids, :], bias=self.fc1.bias[fc1_ids])
-        x = F.relu(x)
-        x = F.linear(
-            x,
-            weight=self.fc2.weight[fc2_ids, :][:, fc1_ids],
-            bias=self.fc2.bias[fc2_ids],
-        )
-        x = F.relu(x)
-        x = F.linear(x, weight=self.fc3.weight[:, fc2_ids], bias=self.fc3.bias)
-        return x
-
-    def forward_dense_rand_ind(self, x, fc1_out, fc2_out, start_id_fc1, start_id_fc2):
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc_base(x)
-        x = F.relu(x)
-        x = F.linear(
-            x,
-            weight=self.fc1.weight[start_id_fc1 : fc1_out + start_id_fc1, :],
-            bias=self.fc1.bias[start_id_fc1 : start_id_fc1 + fc1_out],
-        )
-        x = F.relu(x)
-        x = F.linear(
-            x,
-            weight=self.fc2.weight[
-                start_id_fc2 : start_id_fc2 + fc2_out,
-                start_id_fc1 : start_id_fc1 + fc1_out,
-            ],
-            bias=self.fc2.bias[start_id_fc2 : fc2_out + start_id_fc2],
-        )
-        x = F.relu(x)
-        x = F.linear(
-            x,
-            weight=self.fc3.weight[:, start_id_fc2 : fc2_out + start_id_fc2],
-            bias=self.fc3.bias,
-        )
-        return x
-
     @staticmethod
     def sample_f_in():
         return np.random.randint(1, 121), np.random.randint(1, 85)
-
-    @staticmethod
-    def sample_f_in_and_ids():
-        f1, f2 = np.random.randint(1, 121), np.random.randint(1, 85)
-        start_id_f1 = np.random.randint(0, 121 - f1)
-        start_id_f2 = np.random.randint(0, 85 - f2)
-        return f1, f2, start_id_f1, start_id_f2
-
-    def forward_sparse_lottery(self, x, fc1_out, fc2_out):
-        if self.training:
-            (
-                mask_fc1,
-                mask_fc1_bias,
-                mask_fc2,
-                mask_fc2_bias,
-                mask_fc3,
-                mask_fc3_bias,
-            ) = self.sample_masks_sparse(fc1_out, fc2_out)
-        else:
-            (
-                mask_fc1,
-                mask_fc1_bias,
-                mask_fc2,
-                mask_fc2_bias,
-                mask_fc3,
-                mask_fc3_bias,
-            ) = self.sample_masks_sparse_mag(fc1_out, fc2_out)
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc_base(x)
-        x = F.relu(x)
-        x = F.linear(
-            x, weight=self.fc1.weight * mask_fc1, bias=self.fc1.bias * mask_fc1_bias
-        )
-        x = F.relu(x)
-        x = F.linear(
-            x, weight=self.fc2.weight * mask_fc2, bias=self.fc2.bias * mask_fc2_bias
-        )
-        x = F.relu(x)
-        x = F.linear(
-            x, weight=self.fc3.weight * mask_fc3, bias=self.fc3.bias * mask_fc3_bias
-        )
-        return x
-
-    def forward_sparse_dense_lottery(self, x, fc1_out, fc2_out):
-        (
-            weights_dense_fc1,
-            weights_dense_fc2,
-            weights_dense_fc3,
-            bias_dense_fc1,
-            bias_dense_fc2,
-            bias_dense_fc3,
-        ) = self.sample_sparse_dense_weights(fc1_out, fc2_out)
-        # print(weights_dense_fc1)
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc_base(x)
-        x = F.relu(x)
-        x = F.linear(x, weight=weights_dense_fc1, bias=bias_dense_fc1)
-        x = F.relu(x)
-        x = F.linear(x, weight=weights_dense_fc2, bias=bias_dense_fc2)
-        x = F.relu(x)
-        x = F.linear(x, weight=weights_dense_fc3, bias=bias_dense_fc3)
-        return x
 
     @staticmethod
     def sample_dense_dims():
@@ -200,121 +68,6 @@ class LeNet(nn.Module):
     def sample_min():
         return 1, 1
 
-    @staticmethod
-    def sample_max_ids():
-        return 120, 84, 0, 0
-
-    @staticmethod
-    def sample_min_ids():
-        return 1, 1, np.random.randint(0, 121), np.random.randint(0, 85)
-
-    def sample_masks_sparse(self, fc1_out, fc2_out):
-        mask_fc1 = torch.zeros_like(self.fc1.weight)
-        mask_fc1_bias = torch.zeros_like(self.fc1.bias)
-
-        mask_fc2 = torch.zeros_like(self.fc2.weight)
-        mask_fc2_bias = torch.zeros_like(self.fc2.bias)
-
-        mask_fc3 = torch.zeros_like(self.fc3.weight)
-        mask_fc3_bias = torch.zeros_like(self.fc3.bias)
-
-        # TODO: understand what's happening here
-        indices_fc1 = torch.randperm(mask_fc1.shape[0] * mask_fc1.shape[1])[
-            : fc1_out * 256
-        ]
-        indices_fc1 = torch.stack([indices_fc1 // 256, indices_fc1 % 256], dim=1)
-        mask_fc1[indices_fc1[:, 0], indices_fc1[:, 1]] = 1
-
-        indices_fc1_bias = torch.randperm(mask_fc1_bias.shape[0])[:fc1_out]
-        mask_fc1_bias[indices_fc1_bias] = 1
-
-        indices_fc2 = torch.randperm(mask_fc2.shape[0] * mask_fc2.shape[1])[
-            : fc1_out * fc2_out
-        ]
-        indices_fc2 = torch.stack(
-            [indices_fc2 // mask_fc2.shape[1], indices_fc2 % mask_fc2.shape[1]], dim=1
-        )
-        mask_fc2[indices_fc2[:, 0], indices_fc2[:, 1]] = 1
-
-        indices_fc2_bias = torch.randperm(mask_fc2_bias.shape[0])[:fc2_out]
-        mask_fc2_bias[indices_fc2_bias] = 1
-
-        indices_fc3 = torch.randperm(mask_fc3.shape[0] * mask_fc3.shape[1])[
-            : 10 * fc2_out
-        ]
-        indices_fc3 = torch.stack(
-            [indices_fc3 // mask_fc3.shape[1], indices_fc3 % mask_fc3.shape[1]], dim=1
-        )
-        mask_fc3[indices_fc3[:, 0], indices_fc3[:, 1]] = 1
-
-        indices_fc3_bias = torch.randperm(mask_fc3_bias.shape[0])
-        mask_fc3_bias[indices_fc3_bias] = 1
-
-        return mask_fc1, mask_fc1_bias, mask_fc2, mask_fc2_bias, mask_fc3, mask_fc3_bias
-
-    def sample_masks_sparse_mag(self, fc1_out, fc2_out):
-        # Helper function to create a mask based on weight magnitude
-        def create_mask(weight, n_ones):
-            flattened_weights = weight.view(-1)
-            threshold = torch.topk(
-                flattened_weights.abs(), n_ones, largest=True
-            ).values[-1]
-            mask = (weight.abs() >= threshold).float()
-            return mask
-
-        # Calculate number of ones for each mask
-        n_ones_fc1 = fc1_out * 256
-        n_ones_fc1_bias = fc1_out
-        n_ones_fc2 = fc1_out * fc2_out
-        n_ones_fc2_bias = fc2_out
-        n_ones_fc3 = 10 * fc2_out
-        n_ones_fc3_bias = 10
-
-        # Create masks based on weight magnitude
-        mask_fc1 = create_mask(self.fc1.weight, n_ones_fc1)
-        mask_fc1_bias = create_mask(self.fc1.bias, n_ones_fc1_bias)
-        mask_fc2 = create_mask(self.fc2.weight, n_ones_fc2)
-        mask_fc2_bias = create_mask(self.fc2.bias, n_ones_fc2_bias)
-        mask_fc3 = create_mask(self.fc3.weight, n_ones_fc3)
-        mask_fc3_bias = create_mask(self.fc3.bias, n_ones_fc3_bias)
-
-        return mask_fc1, mask_fc1_bias, mask_fc2, mask_fc2_bias, mask_fc3, mask_fc3_bias
-
-    def sample_sparse_dense_weights(self, fc1_out, fc2_out):
-        if self.training:
-            (
-                mask_fc1,
-                mask_fc1_bias,
-                mask_fc2,
-                mask_fc2_bias,
-                mask_fc3,
-                mask_fc3_bias,
-            ) = self.sample_masks_sparse(fc1_out, fc2_out)
-        else:
-            (
-                mask_fc1,
-                mask_fc1_bias,
-                mask_fc2,
-                mask_fc2_bias,
-                mask_fc3,
-                mask_fc3_bias,
-            ) = self.sample_masks_sparse_mag(fc1_out, fc2_out)
-
-        weights_dense_fc1 = self.fc1.weight[mask_fc1 != 0].reshape(fc1_out, 256)
-        weights_dense_fc2 = self.fc2.weight[mask_fc2 != 0].reshape(fc2_out, fc1_out)
-        weights_dense_fc3 = self.fc3.weight[mask_fc3 != 0].reshape(10, fc2_out)
-        bias_dense_fc1 = self.fc1.bias[mask_fc1_bias != 0].reshape(fc1_out)
-        bias_dense_fc2 = self.fc2.bias[mask_fc2_bias != 0].reshape(fc2_out)
-        bias_dense_fc3 = self.fc3.bias[mask_fc3_bias != 0].reshape(10)
-        return (
-            weights_dense_fc1,
-            weights_dense_fc2,
-            weights_dense_fc3,
-            bias_dense_fc1,
-            bias_dense_fc2,
-            bias_dense_fc3,
-        )
-
     def forward(self, x):
         x = x.reshape(x.shape[0], -1)
         x = self.fc_base(x)
@@ -323,6 +76,3 @@ class LeNet(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-
-
-model = LeNet().to(device=device)
--- examples/fashion_mnist/train_fashion_mnist.py
+++ examples/fashion_mnist/train_fashion_mnist.py
@@ -1,8 +1,10 @@
 from typing import Literal
+from argparse import ArgumentParser
 
+import pandas as pd
+from tabulate import tabulate
 import torch
 import torch.nn as nn
-import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
@@ -11,131 +13,81 @@ from examples.fashion_mnist.model import LeNet
 
 
 def correct(output: torch.Tensor, target: torch.Tensor) -> int:
-    # pick digit with largest network output
+    """Returns the number of correct predictions."""
     predicted_digits = output.argmax(1)
-
-    # 1.0 for correct, 0.0 for incorrect
     correct_ones = (predicted_digits == target).type(torch.float32)
     return correct_ones.sum().item()
 
 
-def sandwich_update_random_id(model: LeNet, batch, criterion, target: torch.Tensor):
-    f1, f2 = model.sample_max()
-    output_largest = model.forward_dense_random_neurons(batch, f1, f2)
-    loss = criterion(output_largest, target)
-    loss.backward()
-
-    f1, f2 = model.sample_min()
-    seed = np.random.randint(0, 10000)
-    output = model.forward_dense_random_neurons(batch, f1, f2, seed=seed)
-    loss = criterion(output, target)
-    loss.backward()
-
-    f1, f2 = model.sample_dense_dims()
-    seed = np.random.randint(0, 10000)
-    output = model.forward_dense_random_neurons(batch, f1, f2, seed=seed)
-    loss = criterion(output, target)
-    loss.backward()
-
-    f1, f2 = model.sample_dense_dims()
-    seed = np.random.randint(0, 10000)
-    output = model.forward_dense_random_neurons(batch, f1, f2, seed=seed)
-    loss = criterion(output, target)
-    loss.backward()
-
-    # for name,param in model.named_parameters():
-    #  param.grad = param.grad/4
-
-    return output, loss
-
-
-def sandwich_update(model, batch, criterion, target):
+def sandwich_update(
+    model: LeNet,
+    batch: torch.Tensor,
+    criterion: nn.CrossEntropyLoss,
+    target: torch.Tensor,
+):
+    """Performs one sandwich rule update."""
     f1, f2 = model.sample_max()
     output = model.forward_dense_lottery(batch, f1, f2)
     loss = criterion(output, target)
     loss.backward()
+
     f1, f2 = model.sample_min()
     output = model.forward_dense_lottery(batch, f1, f2)
     loss = criterion(output, target)
     loss.backward()
+
     f1, f2 = model.sample_dense_dims()
     output = model.forward_dense_lottery(batch, f1, f2)
     loss = criterion(output, target)
     loss.backward()
+
     f1, f2 = model.sample_dense_dims()
     output = model.forward_dense_lottery(batch, f1, f2)
     loss = criterion(output, target)
     loss.backward()
-    # for name,param in model.named_parameters():
-    #  param.grad = param.grad/4
-    return output, loss
 
-
-def sparse_dense_update(model, batch, criterion, target):
-    f1, f2 = model.sample_max()
-    output = model.forward_sparse_dense_lottery(batch, f1, f2)
-    loss = criterion(output, target)
-    loss.backward()
-    f1, f2 = model.sample_min()
-    output = model.forward_sparse_dense_lottery(batch, f1, f2)
-    loss = criterion(output, target)
-    loss.backward()
-    f1, f2 = model.sample_dense_dims()
-    output = model.forward_sparse_dense_lottery(batch, f1, f2)
-    loss = criterion(output, target)
-    loss.backward()
-    f1, f2 = model.sample_dense_dims()
-    output = model.forward_sparse_dense_lottery(batch, f1, f2)
-    loss = criterion(output, target)
-    loss.backward()
-    for name, param in model.named_parameters():
-        param.grad = param.grad / 4
     return output, loss
 
 
 def train(
-    data_loader: DataLoader,
+    dataloader: DataLoader,
     model: LeNet,
     criterion: nn.CrossEntropyLoss,
     optimizer: torch.optim.Optimizer,
     device: torch.device,
-    train_scheme: Literal[
-        "sampling", "sparsify", "sparse_dense", "standard", "sandwich", "sandwich_rand"
+    training_scheme: Literal[
+        "sampling",
+        "standard",
+        "sandwich",
     ] = "sampling",
 ):
+    """Trains the model for one epoch with the given training scheme."""
     model.train()
 
-    num_batches = len(data_loader)
-    num_items = len(data_loader.dataset)
+    num_batches = len(dataloader)
+    num_items = len(dataloader.dataset)
 
     total_loss = 0.0
     total_correct = 0
-    print(train_scheme)
-    for data, target in data_loader:
+    for data, target in dataloader:
         # Copy data and targets to GPU
         data = data.to(device)
         target = target.to(device)
         f1, f2 = model.sample_f_in()
         # Do a forward pass
 
-        match train_scheme:
+        match training_scheme:
             case "standard":
                 output = model(data)
             case "sampling":
                 output = model.forward_dense_lottery(data, f1, f2)
-            case "sparsify":
-                output = model.forward_sparse_lottery(data, f1, f2)
             case "sandwich":
                 output, loss = sandwich_update(model, data, criterion, target)
-            case "sandwich_rand":
-                output, loss = sandwich_update_random_id(model, data, criterion, target)
-            case "sparse_dense":
-                output, loss = sparse_dense_update(model, data, criterion, target)
             case _:
-                raise ValueError(f"Unknown train_scheme: {train_scheme}")
+                raise ValueError(f"Unknown train_scheme: {training_scheme}")
 
         # Calculate the loss
-        if train_scheme not in ["sandwich", "sandwich_rand", "sparse_dense"]:
+        if training_scheme != "sandwich":
             loss = criterion(output, target)
         total_loss += loss
 
@@ -143,7 +95,7 @@ def train(
         total_correct += correct(output, target)
 
         # Backpropagation
-        if train_scheme not in ["sandwich", "sandwich_rand", "sparse_dense"]:
+        if training_scheme != "sandwich":
             loss.backward()
 
         optimizer.step()
@@ -151,16 +103,17 @@ def train(
 
     train_loss = total_loss / num_batches
     accuracy = total_correct / num_items
-    print(f"Average loss: {train_loss:7f}, accuracy: {accuracy:.2%}")
+    print(
+        f"Training scheme: {training_scheme}, Average loss: {train_loss:7f}, accuracy: {accuracy:.2%}"
+    )
 
 
 def test(
     test_loader: DataLoader,
     model: LeNet,
     criterion: nn.CrossEntropyLoss,
-    sparsity_type: Literal["sparse", "dense", "sparse-dense", "dense-rand"] = "sparse",
-    f1_out=10,
-    f2_out=10,
+    f1_out: int,
+    f2_out: int,
 ):
     model.eval()
 
@@ -169,176 +122,108 @@ def test(
 
     test_loss = 0
     total_correct = 0
-    if sparsity_type == "dense-rand":
-        accuracies = []
-        for i in range(10):
-            total_correct = 0
-            total_loss = 0
-            with torch.no_grad():
-                for data, target in test_loader:
-                    data = data.to(device)
-                    target = target.to(device)
-                    output = model.forward_dense_random_neurons(
-                        data, f1_out, f2_out, seed=i
-                    )
-                    total_correct += correct(output, target)
-                    loss = criterion(output, target)
-                    test_loss += loss.item()
-            test_loss = test_loss / num_batches
-            accuracy = total_correct / num_items
-            accuracies.append(accuracy)
-        return max(accuracies)
+    with torch.no_grad():
+        for data, target in test_loader:
+            # Copy data and targets to GPU
+            data = data.to(device)
+            target = target.to(device)
 
-    else:
-        with torch.no_grad():
-            for data, target in test_loader:
-                # Copy data and targets to GPU
-                data = data.to(device)
-                target = target.to(device)
+            output = model.forward_dense_lottery(x=data, fc1_out=f1_out, fc2_out=f2_out)
+            # Calculate the loss
+            loss = criterion(output, target)
+            test_loss += loss.item()
 
-                # Do a forward pass
-                if sparsity_type == "sparse":
-                    output = model.forward_sparse_lottery(data, f1_out, f2_out)
-                if sparsity_type == "dense":
-                    output = model.forward_dense_lottery(data, f1_out, f2_out)
-                if sparsity_type == "sparse-dense":
-                    output = model.forward_sparse_dense_lottery(data, f1_out, f2_out)
-                # Calculate the loss
-                loss = criterion(output, target)
-                test_loss += loss.item()
-
-                # Count number of correct digits
-                total_correct += correct(output, target)
+            # Count number of correct digits
+            total_correct += correct(output, target)
 
     test_loss = test_loss / num_batches
     accuracy = total_correct / num_items
 
-    print(f"Testset accuracy: {100 * accuracy:>0.1f}%, average loss: {test_loss:>7f}")
-    return 100 * accuracy
+    # print(f"Testset accuracy: {100 * accuracy:>0.1f}%, average loss: {test_loss:>7f}")
+    return 100 * accuracy, loss.cpu().item()
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--batch_size", type=int, default=1024)
+    parser.add_argument("--epochs", type=int, default=5)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    args = parser.parse_args()
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     criterion = nn.CrossEntropyLoss()
 
     model_standard = LeNet().to(device)
-    model_random_id = LeNet().to(device)
     model_sampling = LeNet().to(device)
-    model_sparse_dense = LeNet().to(device)
     model_sandwich = LeNet().to(device)
 
-    optimizer_standard = torch.optim.Adam(model_standard.parameters(), lr=1e-3)
-    optimizer_random_id = torch.optim.Adam(model_random_id.parameters(), lr=1e-3)
-    optimizer_sampling = torch.optim.Adam(model_sampling.parameters(), lr=1e-3)
-    optimizer_sparse_dense = torch.optim.Adam(model_sparse_dense.parameters(), lr=1e-3)
-    optimizer_sandwich = torch.optim.Adam(model_sandwich.parameters(), lr=1e-3)
+    optimizer_standard = torch.optim.Adam(model_standard.parameters(), lr=args.lr)
+    optimizer_sampling = torch.optim.Adam(model_sampling.parameters(), lr=args.lr)
+    optimizer_sandwich = torch.optim.Adam(model_sandwich.parameters(), lr=args.lr)
 
-    batch_size = 1024
-    train_dataset = datasets.FashionMNIST(
-        ".", train=True, download=True, transform=ToTensor()
-    )
-    test_dataset = datasets.FashionMNIST(".", train=False, transform=ToTensor())
+    batch_size = args.batch_size
 
     train_loader = DataLoader(
-        dataset=train_dataset, batch_size=batch_size, shuffle=True
+        dataset=datasets.FashionMNIST(
+            ".", train=True, download=True, transform=ToTensor()
+        ),
+        batch_size=batch_size,
+        shuffle=True,
     )
-    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
-    epochs = 5
-    for epoch in range(epochs):
+    test_loader = DataLoader(
+        dataset=datasets.FashionMNIST(".", train=False, transform=ToTensor()),
+        batch_size=batch_size,
+        shuffle=False,
+    )
+
+    for epoch in range(args.epochs):
         print(f"Training epoch: {epoch + 1}")
         train(
-            train_loader,
-            model_standard,
-            criterion,
-            optimizer_standard,
+            dataloader=train_loader,
+            model=model_standard,
+            criterion=criterion,
+            optimizer=optimizer_standard,
             device=device,
-            train_scheme="standard",
+            training_scheme="standard",
         )
         train(
-            train_loader,
-            model_sampling,
-            criterion,
-            optimizer_sampling,
+            dataloader=train_loader,
+            model=model_sampling,
+            criterion=criterion,
+            optimizer=optimizer_sampling,
             device=device,
-            train_scheme="sampling",
+            training_scheme="sampling",
         )
         train(
-            train_loader,
-            model_sparse_dense,
-            criterion,
-            optimizer_sparse_dense,
+            dataloader=train_loader,
+            model=model_sandwich,
+            criterion=criterion,
+            optimizer=optimizer_sandwich,
             device=device,
-            train_scheme="sparse_dense",
-        )
-        train(
-            train_loader,
-            model_random_id,
-            criterion,
-            optimizer_random_id,
-            device=device,
-            train_scheme="sandwich_rand",
-        )
-        train(
-            train_loader,
-            model_sandwich,
-            criterion,
-            optimizer_sandwich,
-            device=device,
-            train_scheme="sandwich",
+            training_scheme="sandwich",
         )
 
     lottery_grid = [[25, 25], [50, 50], [120, 84]]
-    training_schemes = ["standard", "sampling", "sandwich", "sandwich_rand"]
-    lottery_sparse = {}
-    lottery_dense = {}
-    lottery_sparse_dense = {}
-    lottery_dense_rand = {}
-    for scheme in training_schemes:
-        lottery_sparse[scheme] = {}
-        lottery_dense[scheme] = {}
-        lottery_sparse[scheme] = {}
-        lottery_dense_rand[scheme] = {}
+    training_schemes = ["standard", "sampling", "sandwich"]
+    lottery_dense = {scheme: {} for scheme in training_schemes}
     for k in lottery_grid:
         for scheme in training_schemes:
             if scheme == "standard":
                 model = model_standard
             elif scheme == "sampling":
                 model = model_sampling
-            elif scheme == "sparse-dense":
-                model = model_sparse_dense
-            elif scheme == "sandwich_rand":
-                model = model_random_id
             else:
                 model = model_sandwich
-            lottery_sparse[scheme][str(k[0] * k[1])] = test(
+
+            acc, loss = test(
                 test_loader,
                 model,
                 criterion,
-                sparsity_type="sparse",
                 f1_out=k[0],
                 f2_out=k[1],
             )
-            lottery_dense[scheme][str(k[0] * k[1])] = test(
-                test_loader,
-                model,
-                criterion,
-                sparsity_type="dense",
-                f1_out=k[0],
-                f2_out=k[1],
-            )
-            lottery_dense_rand[scheme][str(k[0] * k[1])] = test(
-                test_loader,
-                model,
-                criterion,
-                sparsity_type="dense-rand",
-                f1_out=k[0],
-                f2_out=k[1],
-            )
-            lottery_sparse_dense[scheme][str(k[0]) + "_" + str(k[1])] = test(
-                test_loader,
-                model,
-                criterion,
-                sparsity_type="sparse-dense",
-                f1_out=k[0],
-                f2_out=k[1],
-            )
+            lottery_dense[scheme][f"{str(k[0] * k[1])}_accuracy"] = acc
+            lottery_dense[scheme][f"{str(k[0] * k[1])}_loss"] = loss
+
+    df = pd.DataFrame(lottery_dense)
+    print(tabulate(df, headers="keys", tablefmt="pipe", floatfmt=".4f"))
