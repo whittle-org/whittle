19293eb0c5d37ed3a21323f5dcfa74e0ca983062
Aaron <kleiaaro@gmail.com>
2024-08-12 13:31:37 +0200

    address comments

--- test/test_mag.py
+++ test/test_mag.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 from litgpt.config import Config
@@ -6,7 +7,13 @@ from whittle.models.gpt import GPT
 from whittle.metrics.mag import weight_magnitude
 
 
-def test_weight_magnitude():
+mlp_types = ["GptNeoxMLP", "LLaMAMLP", "GemmaMLP"]
+norm_types = ["LayerNorm", "RMSNorm"]
+
+
+@pytest.mark.parametrize("mlp_type", mlp_types)
+@pytest.mark.parametrize("norm_type", norm_types)
+def test_weight_magnitude(mlp_type, norm_type):
     config = Config()
     config.padded_vocab_size = 512
     config.n_embd = 64
@@ -20,27 +27,41 @@ def test_weight_magnitude():
     config.norm_eps = 1e-5
     config.lm_head_bias = True
     config.fix_head_size = True
+    config.mlp_class_name = mlp_type
+    config.norm_class_name = norm_type
 
     gpt = GPT(config)
     gpt.transformer.wte.weight.data = torch.ones_like(gpt.transformer.wte.weight.data)
     gpt.lm_head.weight.data = torch.ones_like(gpt.lm_head.weight.data)
     gpt.lm_head.bias.data = torch.ones_like(gpt.lm_head.bias.data)
     gpt.transformer.ln_f.weight.data = torch.ones_like(gpt.transformer.ln_f.weight.data)
-    gpt.transformer.ln_f.bias.data = torch.ones_like(gpt.transformer.ln_f.bias.data)
+    if norm_type == "LayerNorm":
+        gpt.transformer.ln_f.bias.data = torch.ones_like(gpt.transformer.ln_f.bias.data)
 
     for block in gpt.transformer.h:
         block.attn.attn.weight.data = torch.ones_like(block.attn.attn.weight.data)
         block.attn.attn.bias.data = torch.ones_like(block.attn.attn.bias.data)
         block.attn.proj.bias.data = torch.ones_like(block.attn.proj.bias.data)
         block.attn.proj.weight.data = torch.ones_like(block.attn.proj.weight.data)
-        block.mlp.fc.weight.data = torch.ones_like(block.mlp.fc.weight.data)
-        block.mlp.fc.bias.data = torch.ones_like(block.mlp.fc.bias.data)
-        block.mlp.proj.weight.data = torch.ones_like(block.mlp.proj.weight.data)
-        block.mlp.proj.bias.data = torch.ones_like(block.mlp.proj.bias.data)
+
+        if mlp_type == "GptNeoxMLP":
+            block.mlp.fc.weight.data = torch.ones_like(block.mlp.fc.weight.data)
+            block.mlp.fc.bias.data = torch.ones_like(block.mlp.fc.bias.data)
+            block.mlp.proj.weight.data = torch.ones_like(block.mlp.proj.weight.data)
+            block.mlp.proj.bias.data = torch.ones_like(block.mlp.proj.bias.data)
+        else:
+            block.mlp.fc_1.weight.data = torch.ones_like(block.mlp.fc_1.weight.data)
+            block.mlp.fc_1.bias.data = torch.ones_like(block.mlp.fc_1.bias.data)
+            block.mlp.fc_2.weight.data = torch.ones_like(block.mlp.fc_2.weight.data)
+            block.mlp.fc_2.bias.data = torch.ones_like(block.mlp.fc_2.bias.data)
+            block.mlp.proj.weight.data = torch.ones_like(block.mlp.proj.weight.data)
+            block.mlp.proj.bias.data = torch.ones_like(block.mlp.proj.bias.data)
+
         block.norm_1.weight.data = torch.ones_like(block.norm_1.weight.data)
         block.norm_2.weight.data = torch.ones_like(block.norm_2.weight.data)
-        block.norm_1.bias.data = torch.ones_like(block.norm_1.bias.data)
-        block.norm_2.bias.data = torch.ones_like(block.norm_2.bias.data)
+        if norm_type == "LayerNorm":
+            block.norm_1.bias.data = torch.ones_like(block.norm_1.bias.data)
+            block.norm_2.bias.data = torch.ones_like(block.norm_2.bias.data)
     mag_super = weight_magnitude(gpt)
 
     num_params_super = sum(p.numel() for p in gpt.parameters() if p.requires_grad)
--- whittle/metrics/mag.py
+++ whittle/metrics/mag.py
@@ -1,26 +1,67 @@
 import torch
 
+import logging
+
+from whittle.modules.layernorm import LayerNorm
+from whittle.modules.rmsnorm import RMSNorm
 from whittle.models.gpt import GPT
+from whittle.models.gpt.blocks import GptNeoxMLP, GemmaMLP, LLaMAMLP
 
 
 def weight_magnitude(model: GPT):
+    """
+    Computes the sum of the weight magnitudes of the current sub-network of a GPT model. Make sure to set the
+    sub-network before calling this function.
+
+    Args:
+        model: GPT model
+
+    Returns:
+        float: magnitude of the weights of the activated sub-network
+    """
     magnitude = 0
     magnitude += weight_magnitude_linear_layer(model.lm_head)
     magnitude += weight_magnitude_embedding(model.transformer.wte)
-    for block in model.transformer.h:
+    for i in range(model.sub_network_n_layers):
+        block = model.transformer.h[i]
         magnitude += weight_magnitude_layer_norm(block.norm_1)
         magnitude += weight_magnitude_attention(block.attn)
-        magnitude += weight_magnitude_linear_layer(block.mlp.fc)
-        magnitude += weight_magnitude_linear_layer(block.mlp.proj)
+        magnitude += weight_magnitude_mlp(block.mlp)
         magnitude += weight_magnitude_layer_norm(block.norm_2)
     magnitude += weight_magnitude_layer_norm(model.transformer.ln_f)
     return magnitude
 
 
+def weight_magnitude_mlp(mlp):
+    if isinstance(mlp, GptNeoxMLP):
+        layers = [mlp.proj, mlp.fc]
+
+    elif isinstance(mlp, LLaMAMLP) or isinstance(mlp, GemmaMLP):
+        layers = [mlp.proj, mlp.fc_1, mlp.fc_2]
+
+    else:
+        logging.error(f"MLP type: {type(mlp)} not supported!")
+        raise
+
+    mag = 0
+    for layer in layers:
+        mag += weight_magnitude_linear_layer(layer)
+    return mag
+
+
 def weight_magnitude_layer_norm(layer):
-    n = layer.sub_network_in_features
-    mag = torch.sum(torch.abs(layer.weight[:n]))
-    mag += torch.sum(torch.abs(layer.bias[:n]))
+    if isinstance(layer, LayerNorm):
+        n = layer.sub_network_in_features
+        mag = torch.sum(torch.abs(layer.weight[:n]))
+        mag += torch.sum(torch.abs(layer.bias[:n]))
+
+    elif isinstance(layer, RMSNorm):
+        n = layer.sub_network_in_features
+        mag = torch.sum(torch.abs(layer.weight[:n]))
+    else:
+        logging.error(f"Normalization layer type: {type(layer)} not supported!")
+        raise
+
     return float(mag)
 
 
