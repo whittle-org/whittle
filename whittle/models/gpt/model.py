eff9963b0c77381d4ef404df070a9cf1f8054620
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-11-05 11:55:43 +0100

    Update model.py

--- whittle/models/gpt/model.py
+++ whittle/models/gpt/model.py
@@ -175,8 +175,8 @@ class GPT(nn.Module):
         sub_network_intermediate_size: int,
         sub_network_num_heads: int,
         sub_network_n_layers: int,
-        sub_network_query_groups: Optional[int] = None,
-        sub_network_head_size: Optional[int] = None,
+        sub_network_query_groups = None,
+        sub_network_head_size = None,
     ) -> None:
         self.sub_network_head_size = sub_network_head_size
         self.sub_network_n_embd = sub_network_n_embd
