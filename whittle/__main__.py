0aab4d93af715397c4ad458f800760d6e62a3a5a
timurcarstensen <timurcarstensen@gmail.com>
2025-02-13 13:33:49 +0100

    fix: switching to new evaluate function

--- whittle/__main__.py
+++ whittle/__main__.py
@@ -9,7 +9,7 @@ from litgpt.chat.base import main as chat_fn
 from litgpt.deploy.serve import run_server as serve_fn
 from litgpt.scripts.download import download_from_hub as download_fn
 
-from whittle.eval.utils import convert_and_evaluate as evaluate_fn
+from whittle.evaluate_network import setup as evaluate_fn
 from whittle.finetune import setup as finetune_fn
 from whittle.pretrain_super_network import setup as pretrain_fn
 from whittle.search_sub_networks import setup as search_fn
