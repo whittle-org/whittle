5f8a0a24232d45259021628daf45b877edb2726c
timurcarstensen <timurcarstensen@gmail.com>
2025-02-13 13:38:51 +0100

    fix: syne-tune random search import

--- whittle/search/stratified_search.py
+++ whittle/search/stratified_search.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from typing import Any
 
 from syne_tune.optimizer.schedulers import FIFOScheduler
-from syne_tune.optimizer.schedulers.searchers.random_grid_searcher import RandomSearcher
+from syne_tune.optimizer.schedulers.searchers import (
+    LegacyRandomSearcher as RandomSearcher,
+)
 
 from whittle.sampling.param_bins import ParamBins
 
