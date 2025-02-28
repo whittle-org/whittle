6ea8cd45be57584fd95c7d8c710e62d766518233
Hannan Mahadik <hannanm@outlook.de>
2025-02-28 13:26:17 +0100

    updated args.py - indentation

--- whittle/args.py
+++ whittle/args.py
@@ -22,12 +22,12 @@ class DistillArgs:
     """Distillation-related arguments
 
     Args:
-       method: Distillation method to use ('logits' or 'hidden_states') - Only supports 'logits' for now
-       temperature: Controls softening of output probabilities. Higher values (>1) produce softer distributions,
-         emphasizing less confident predictions. Lower values (<1) make distributions sharper, focusing on
-         confident predictions.
-       alpha: Weight balancing distillation loss vs cross-entropy loss. Values closer to 1 give more importance
-         to matching teacher logits, while values closer to 0 prioritize true label prediction.
+        method: Distillation method to use ('logits' or 'hidden_states') - Only supports 'logits' for now
+        temperature: Controls softening of output probabilities. Higher values (>1) produce softer distributions,
+          emphasizing less confident predictions. Lower values (<1) make distributions sharper, focusing on
+          confident predictions.
+        alpha: Weight balancing distillation loss vs cross-entropy loss. Values closer to 1 give more importance
+          to matching teacher logits, while values closer to 0 prioritize true label prediction.
     """
 
     method: str = "logits"
