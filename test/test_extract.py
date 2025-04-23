162109ee376469cdd4569db43ff2434faa8f7b1c
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2025-04-23 09:30:44 +0200

    Update test_extract.py

--- test/test_extract.py
+++ test/test_extract.py
@@ -69,7 +69,7 @@ def test_extract_sub_network_llamamlp_gqa() -> None:
     n_embd = 128
     intermediate_size = 1024
     n_layer = 6
-    n_head = 8
+    n_head = 6
     n_query_groups = 3
 
     super_network.eval()
@@ -189,3 +189,4 @@ def make_norm_weights_random(norm):
     torch.nn.init.normal_(norm.weight)
     if isinstance(norm, LayerNorm):
         torch.nn.init.normal_(norm.bias)
+
