ce7cf7bcea3bea8dfae8325112644034c54060e4
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2025-08-15 13:20:55 +0200

    fix: fix subnet nheads (#324)

--- whittle/models/gpt/model.py
+++ whittle/models/gpt/model.py
@@ -202,30 +202,12 @@ class GPT(nn.Module):
         self.sub_network_n_layers = sub_network_n_layers
         self.transformer.wte.set_sub_network(self.sub_network_n_embd)
         self.transformer.ln_f.set_sub_network(self.sub_network_n_embd)
-        if self.config.n_query_groups == 1:
-            self.sub_network_query_groups = 1
-            self.sub_network_num_heads = (
-                sub_network_num_heads
-                if sub_network_num_heads is not None
-                else self.config.n_head
-            )
-        elif self.config.n_head != self.config.n_query_groups:
-            self.sub_network_num_heads = (
-                sub_network_num_heads
-                if sub_network_num_heads is not None
-                else self.config.n_head
-            )
-            self.sub_network_query_groups = (
-                sub_network_query_groups
-                if sub_network_query_groups is not None
-                else self.config.n_query_groups
-            )
-        else:
-            self.sub_network_query_groups = (
-                sub_network_query_groups
-                if sub_network_query_groups is not None
-                else self.config.n_head
-            )
+        self.sub_network_query_groups = (
+            sub_network_query_groups
+            if sub_network_query_groups is not None
+            else self.config.n_query_groups
+        )
+
         if self.config.fix_head_size:
             if sub_network_head_size is None:
                 self.sub_network_head_size = self.config.head_size
