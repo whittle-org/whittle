fix: fix qk normalization in attention layers (#365)

whittle/models/gpt/blocks/causal_self_attention.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import math
+from functools import partial
 from typing import Any
 
 import torch
@@ -11,6 +12,8 @@ from litgpt.scripts.convert_hf_checkpoint import qkv_reassemble
 
 from whittle.exceptions import IllegalSubNetworkError
 from whittle.modules import Linear
+from whittle.modules.layernorm import LayerNorm
+from whittle.modules.rmsnorm import RMSNorm
 
 
 class CausalSelfAttention(nn.Module):
@@ -47,8 +50,9 @@ class CausalSelfAttention(nn.Module):
                 if config.norm_qk_type == "olmo2"
                 else config.head_size
             )
-            self.norm_q = config.norm_class(norm_q_size, eps=config.norm_eps)
-            self.norm_k = config.norm_class(norm_k_size, eps=config.norm_eps)
+            norm_class = self.norm_class()
+            self.norm_q = norm_class(norm_q_size, eps=config.norm_eps)
+            self.norm_k = norm_class(norm_k_size, eps=config.norm_eps)
         else:
             self.norm_q = self.norm_k = None
         # Set current sub-network to super-network
@@ -64,6 +68,19 @@ class CausalSelfAttention(nn.Module):
             self.sub_network_n_head // self.sub_network_query_groups
         )
         self.sub_attention_scaler = self.config.attention_scores_scalar
+
+    def norm_class(self):
+        # `self._norm_class` cannot be the type to keep the config json serializable
+        if self.config.norm_class_name == "RMSNorm":
+            return partial(RMSNorm, add_unit_offset="Gemma" in self.config.name)
+
+        if self.config.norm_class_name == "LayerNorm" and "OLMo" in self.config.name:
+            # this makes it equivalent to `torch.nn.functional.layer_norm`
+            # that is used by OLMo
+            # Table 5 caption in the OLMo paper shows this - https://aclanthology.org/2024.acl-long.841
+            return partial(torch.nn.LayerNorm, elementwise_affine=False)
+
+        return LayerNorm
 
     def _verify_subnet_is_legal(
         self,

whittle/modules/embedding.py
@@ -20,15 +20,15 @@ class Embedding(torch.nn.Embedding):
         dtype=None,
     ) -> None:
         super().__init__(
-            num_embeddings,
-            embedding_dim,
-            padding_idx,
-            max_norm,
-            norm_type,
-            scale_grad_by_freq,
-            sparse,
-            device,
-            dtype,
+            num_embeddings=num_embeddings,
+            embedding_dim=embedding_dim,
+            padding_idx=padding_idx,
+            max_norm=max_norm,
+            norm_type=norm_type,
+            scale_grad_by_freq=scale_grad_by_freq,
+            sparse=sparse,
+            device=device,
+            dtype=dtype,
         )
 
         # the embedding dimensionality of the current sub-network

whittle/modules/layernorm.py
@@ -7,8 +7,23 @@ import torch.nn.functional as F
 class LayerNorm(torch.nn.LayerNorm):
     """An extension of PyTorch's `torch.nn.LayerNorm` with support  with support to sub-sample weights corresponding to the sub-network dimensionality."""
 
-    def __init__(self, in_features: int, eps: float = 1e-5):
-        super().__init__(in_features, eps)
+    def __init__(
+        self,
+        in_features: int,
+        eps: float = 1e-5,
+        elementwise_affine: bool = True,
+        bias: bool = True,
+        device=None,
+        dtype=None,
+    ):
+        super().__init__(
+            normalized_shape=in_features,
+            eps=eps,
+            elementwise_affine=elementwise_affine,
+            bias=bias,
+            device=device,
+            dtype=dtype,
+        )
         self.in_features = in_features
 
         # Set current sub-network to super-network

whittle/modules/linear.py
@@ -16,7 +16,13 @@ class Linear(nn.Linear):
         device=None,
         dtype=None,
     ):
-        super().__init__(in_features, out_features, bias, device, dtype)
+        super().__init__(
+            in_features=in_features,
+            out_features=out_features,
+            bias=bias,
+            device=device,
+            dtype=dtype,
+        )
 
         # Set the current sub-network dimensions equal to super-network
         self.sub_network_in_features = in_features