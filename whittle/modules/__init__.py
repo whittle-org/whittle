aafc835c3601c00f2cc8e8ab08a8a198e9aa8f2e
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2026-07-29 19:42:34 +0200

    feat: expose the sub-network API at package level

--- whittle/modules/__init__.py
+++ whittle/modules/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from .embedding import Embedding
+from .layernorm import LayerNorm
 from .linear import Linear
+from .rmsnorm import RMSNorm
 
-__all__ = ["Linear"]
+__all__ = ["Embedding", "LayerNorm", "Linear", "RMSNorm"]
